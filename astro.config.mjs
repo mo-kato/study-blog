00377fe236d324b370ca9d19b6cb47db9e8a3bdd
m.kato <169634836+mo-kato@users.noreply.github.com>
2026-07-08 18:08:35 +0900

    chore: base URL変えてみる実験

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  site: "https://mo-kato.github.io",
-  base: "/study-blog",
+  site: "https://mo-kato.github.io/study-blog",
 });
