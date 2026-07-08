5ed4d0a1f3bc59123e7eed0a958c1429dfe29de2
m.kato <169634836+mo-kato@users.noreply.github.com>
2026-07-08 18:10:42 +0900

    Revert "chore: base URL変えてみる実験"
    This reverts commit 00377fe236d324b370ca9d19b6cb47db9e8a3bdd.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  site: "https://mo-kato.github.io/study-blog",
+  site: "https://mo-kato.github.io",
+  base: "/study-blog",
 });
